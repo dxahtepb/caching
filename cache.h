66ab432a258b52c3b886ed181bbb6ce727134431
Andrey Chausov <dxahtepb@gmail.com>
2019-02-04 16:42:57 +0300

    buggy CART cache

--- cache.h
+++ cache.h
@@ -239,7 +239,8 @@ public:
         history_frequency_(),
         history_recency_(),
         target_size_(0),
-        cache_misses_(0)
+        cache_misses_(0),
+        data_map_()
     {
     }
 
@@ -429,6 +430,213 @@ private:
     }
 };
 
+template<typename Key, typename Value, typename EntryAlloc>
+class CartCache
+{
+    struct Entry
+    {
+        char filter_bit;
+        int access_bit;
+        bool is_history;
+        Value value;
+    };
+public:
+    explicit
+    CartCache(size_t capacity)
+            : capacity_(capacity),
+              entry_alloc_(),
+              cache_size_(capacity / 2),
+              cache_recency_(),
+              cache_frequency_(),
+              history_frequency_(),
+              history_recency_(),
+              target_cache_size_(0),
+              target_history_size_(0),
+              long_pages_count_(0),
+              short_pages_count_(0),
+              cache_misses_(0),
+              data_map_()
+    {
+    }
+
+    Value get(Key key)
+    {
+        if (!check_cache_presence(key))
+        {
+            handle_cache_miss(key);
+        }
+        else
+        {
+            data_map_[key].access_bit = true;
+        }
+
+        return data_map_[key].value;
+    }
+
+    bool check_cache_presence(Key const & key) const
+    {
+        if (data_map_.find(key) != data_map_.end())
+        {
+            if (!data_map_.at(key).is_history)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    auto get_contents_keys()
+    {
+        std::vector<Key> keys;
+        keys.reserve(data_map_.size());
+        for (auto & item : data_map_)
+        {
+            keys.push_back(item.first);
+        }
+        return keys;
+    }
+
+    uint64_t get_cache_misses()
+    {
+        return cache_misses_;
+    }
+
+
+private:
+    size_t capacity_;
+    size_t cache_size_;
+    size_t target_cache_size_;
+    size_t target_history_size_;
+    size_t long_pages_count_;
+    size_t short_pages_count_;
+
+    SecondChanceList<Key> cache_recency_;
+    SecondChanceList<Key> cache_frequency_;
+    LruList<Key> history_recency_;
+    LruList<Key> history_frequency_;
+
+    EntryAlloc entry_alloc_;
+
+    uint64_t cache_misses_;
+
+    std::unordered_map<Key, Entry> data_map_;
+
+    void evict_from_cache()
+    {
+        while (cache_frequency_.size()
+               && data_map_[cache_frequency_.head()].access_bit == 1)
+        {
+            auto frequency_head = cache_frequency_.head();
+            cache_recency_.push(frequency_head);
+            cache_frequency_.remove();
+            data_map_[frequency_head].access_bit = 0;
+            if (cache_frequency_.size() + history_frequency_.size()
+                + cache_recency_.size() - short_pages_count_ >= cache_size_)
+            {
+                target_history_size_ = std::min(target_history_size_ + 1, 2 * cache_size_ - cache_recency_.size());
+            }
+        }
+
+        while (cache_recency_.size()
+               && (data_map_[cache_recency_.head()].filter_bit == 'L'
+                   || data_map_[cache_recency_.head()].access_bit == 1))
+        {
+            if (data_map_[cache_recency_.head()].access_bit == 1)
+            {
+                auto moved_page = cache_recency_.head();
+                cache_recency_.push(moved_page);
+                cache_recency_.remove();
+                data_map_[moved_page].access_bit == 0;
+                if (cache_recency_.size() >= std::min(target_cache_size_ + 1, history_recency_.size())
+                    && data_map_[moved_page].filter_bit == 'S')
+                {
+                    data_map_[moved_page].filter_bit = 'L';
+                    ++long_pages_count_;
+                    --short_pages_count_;
+                }
+            }
+            else
+            {
+                auto moved_page = cache_recency_.head();
+                cache_recency_.remove();
+                cache_frequency_.push(moved_page);
+                data_map_[moved_page].access_bit = 0;
+                target_history_size_ = std::max(target_history_size_ - 1, cache_size_ - cache_recency_.size());
+            }
+        }
+
+        if (cache_recency_.size() >= std::max((size_t) 1ULL, target_cache_size_))
+        {
+            history_recency_.make_mru(cache_recency_.head());
+            cache_recency_.remove();
+            --short_pages_count_;
+        }
+        else
+        {
+            history_frequency_.make_mru(cache_frequency_.head());
+            cache_frequency_.remove();
+            --long_pages_count_;
+        }
+    }
+
+    void handle_cache_miss(Key key)
+    {
+        if (cache_frequency_.size() + cache_recency_.size() == cache_size_)
+        {
+            evict_from_cache();
+
+            if (!history_recency_.check_presence(key) && !history_frequency_.check_presence(key))
+            {
+                if (history_recency_.size() > std::max((size_t) 0, target_history_size_)
+                    || history_frequency_.size() == 0)
+                {
+                    auto removed_entry = history_recency_.remove_lru();
+                    data_map_.erase(removed_entry);
+                }
+                else if (history_recency_.size() + history_frequency_.size() == cache_size_ + 1)
+                {
+                    auto removed_entry = history_frequency_.remove_lru();
+                    data_map_.erase(removed_entry);
+                }
+            }
+        }
+
+        if (!history_frequency_.check_presence(key) && !history_recency_.check_presence(key))
+        {
+            data_map_.insert({key, {'S', 0, false, entry_alloc_(key)}});
+            ++cache_misses_;
+            ++short_pages_count_;
+            cache_recency_.push(key);
+        }
+        else if (history_recency_.check_presence(key))
+        {
+            const unsigned long long growth_factor = short_pages_count_ / history_recency_.size();
+            target_cache_size_ = std::min(
+                    target_cache_size_ + std::max(1ULL, growth_factor),
+                    (unsigned long long) cache_size_);
+            history_recency_.erase(key);
+            cache_recency_.push(key);
+            data_map_[key].access_bit = 0;
+            data_map_[key].filter_bit = 'L';
+            ++long_pages_count_;
+        }
+        else if (history_frequency_.check_presence(key))
+        {
+            const unsigned long long growth_factor = long_pages_count_ / history_frequency_.size();
+            target_cache_size_ = std::max(target_cache_size_ - std::max(1ULL, growth_factor), 0ULL);
+            history_frequency_.erase(key);
+            cache_recency_.push(key);
+            data_map_[key].access_bit = 0;
+            ++long_pages_count_;
+            if (cache_frequency_.size() + history_frequency_.size()
+                + cache_recency_.size() - short_pages_count_ >= cache_size_)
+            {
+                target_history_size_ = std::min(target_history_size_ + 1, 2 * cache_size_ - cache_recency_.size());
+            }
+        }
+    }
+
+};
 
 
 #endif //CACHINGPP_CACHE_H
